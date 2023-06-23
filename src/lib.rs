a34e093db664ff9d3288760aafd321d6be9a20d8
Stephen Kuenzli <skuenzli@k9security.io>
2023-06-23 12:19:53 -0700

    Add a pytest that verifies the cedarpolicy can invoke functions in cedar (parse a test policy)

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,13 @@
+use std::path::Path;
+use std::str::FromStr;
 use pyo3::prelude::*;
+
+use anyhow::{Context as _, Error, Result};
+use cedar_policy::{Policy, PolicyId, PolicySet};
+use cedar_policy::PrincipalConstraint::{Any, Eq, In};
+// use cedar_policy::*;
+use cedar_policy_formatter::{policies_str_to_pretty, Config};
+use pyo3::exceptions::PyRuntimeError;
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
@@ -6,9 +15,76 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
 
+// #[pyfunction]
+// fn format_policies(file_name: String, line_width: usize, indent_width: isize) -> PyResult<()> {
+//     let policies_str = read_from_file_or_stdin(Some(file_name).as_ref(),
+//                                                "policy set")?;
+//     let config = Config {
+//         line_width,
+//         indent_width,
+//     };
+//     println!("{}", policies_str_to_pretty(&policies_str, &config)?);
+//     Ok(())
+// }
+
+#[pyfunction]
+fn parse_test_policy() -> PyResult<String>{
+    println!("Example: Parsing a Cedar Policy");
+    // this policy has a type error, but parses.
+    let src = r#"
+    permit(
+        principal == User::"bob",
+        action == Action::"view",
+        resource
+    )
+    when { 10 > "hello" };
+"#;
+    let p = PolicySet::from_str(src);
+    match p {
+        Ok(pset) => {
+            let pid = PolicyId::from_str("policy_id_00").unwrap();
+            let policy = PolicySet::policy(&pset, &pid);
+            if let Some(p) = policy {
+                println!("Policy:{}", p);
+                let pr = Policy::principal_constraint(p);
+                match pr {
+                    Any => println!("No Principal"),
+                    In(euid) => println!("Principal Constraint: Principal in {}", euid),
+                    Eq(euid) => println!("Principal Constraint: Principal=={}", euid),
+                }
+            }
+            return Ok(String::from("Ok!"));
+        }
+        Err(e) => {
+            println!("{:?}", e);
+            return Err(PyRuntimeError::new_err("Could nor parse test policy :("));
+        }
+    }
+}
+
+// Read from a file (when `filename` is a `Some`) or stdin (when `filename` is `None`)
+fn read_from_file_or_stdin(filename: Option<impl AsRef<Path>>, context: &str) -> Result<String> {
+    let mut src_str = String::new();
+    match filename.as_ref() {
+        Some(path) => {
+            src_str = std::fs::read_to_string(path).context(format!(
+                "failed to open {} file {}",
+                context,
+                path.as_ref().display()
+            ))?;
+        }
+        None => {
+            std::io::Read::read_to_string(&mut std::io::stdin(), &mut src_str)
+                .context(format!("failed to read {} from stdin", context))?;
+        }
+    };
+    Ok(src_str)
+}
+
 /// A Python module implemented in Rust.
 #[pymodule]
 fn cedarpolicy(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(parse_test_policy, m)?)?;
     Ok(())
 }
--- tests/unit/test_import_module.py
+++ tests/unit/test_import_module.py
@@ -8,3 +8,11 @@ class ImportModuleTestCase(unittest.TestCase):
         import cedarpolicy
         # successfully imported cedarpolicy module
         self.assertTrue(True)
+
+
+class InvokeModuleTestFunctionTestCase(unittest.TestCase):
+
+    def test_invoke_module_test_function(self):
+        import cedarpolicy
+        result = cedarpolicy.parse_test_policy()
+        self.assertEqual('Ok!', result)
