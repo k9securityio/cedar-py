931d59302c91f173d84f7b82674dc0c3aef499a0
Stephen Kuenzli <skuenzli@k9security.io>
2023-07-03 14:45:13 -0700

    Start documenting is_authorized

--- cedarpolicy/__init__.py
+++ cedarpolicy/__init__.py
@@ -16,5 +16,17 @@ def is_authorized(request: dict,
                   entities: str,
                   schema: str = None,
                   verbose: bool = False) -> dict:
+    """Evaluate whether the request is authorized given the parameters.
+
+    :returns a dictionary containing the decision and diagnostic details, e.g.:
+    {
+        "decision": "Allow|Deny",
+        "diagnostics": {
+            "reason": ["policy0"], # the policy that determined the decision
+            "errors": []           # any errors
+        }
+    }
+
+    """
     authz_response = _cedarpolicy.is_authorized(request, policies, entities, schema, verbose)
     return json.loads(authz_response)
