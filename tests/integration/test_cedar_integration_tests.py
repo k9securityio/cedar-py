9a42f175e777ce0c0ff1a9fc24f2d9a3fa911b23
Stephen Kuenzli <skuenzli@k9security.io>
2023-07-05 11:02:47 -0700

    Add tests for use cases 5b (Passing)

--- tests/integration/test_cedar_integration_tests.py
+++ tests/integration/test_cedar_integration_tests.py
@@ -203,6 +203,19 @@ class CedarIntegrationTestCase(unittest.TestCase):
                                               should_validate=should_validate,
                                               query=query)
 
+    @parameterized.expand(get_authz_test_params_for_use_case("5b"),
+                          name_func=custom_name_func)
+    def test_example_use_cases_doc_5b(self,
+                                      policies: str,
+                                      entities: list,
+                                      schema: dict,
+                                      should_validate: bool,  # ignored; currently don't have the equivalent
+                                      query: dict):
+
+        self.exec_authz_query_with_assertions(policies=policies, entities=entities, schema=schema,
+                                              should_validate=should_validate,
+                                              query=query)
+
     def exec_authz_query_with_assertions(self,
                                          policies: str,
                                          entities: list,
